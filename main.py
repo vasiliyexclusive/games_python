74a7f2e048a7648a56c5fadc0ec7e3021818fdff
vasiliyexclusive <jajababajaja@gmail.com>
2025-04-07 11:57:22 +0300

    вспоминаю add продолжаем

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import pygame
 
-SIZE_BLOCK = 20
-FRAME_COLOR = (50, 50, 204)
+SIZE_BLOCK = 35
+FRAME_COLOR = (0, 255, 204)
 WHITE = (255, 255, 255)
 BLUE = (204, 255, 255)
+HEADER_COLOR = (0, 204, 153)
+SNAKE_COLOR = (0, 102, 0)
 COUNT_BLOCKS = 20
+HEADER_MARGIN = 70
 MARGIN = 1
 
-size = [400, 600]
+
+size = [SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS,
+        SIZE_BLOCK * COUNT_BLOCKS + 2 * SIZE_BLOCK + MARGIN * COUNT_BLOCKS + HEADER_MARGIN]
+print(size)
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Змеюка')
+
+def draw_block(color, row, column):
+    pygame.draw.rect(screen, color, [SIZE_BLOCK + column * SIZE_BLOCK + MARGIN * (column + 1),
+                                     HEADER_MARGIN + SIZE_BLOCK + row * SIZE_BLOCK + MARGIN * (row + 1),
+                                     SIZE_BLOCK,
+                                     SIZE_BLOCK])
 
 while True:
     for event in pygame.event.get():
@@ -18,16 +30,20 @@ while True:
             print('exit')
             pygame.quit()
 
+
     screen.fill(FRAME_COLOR)
 
-    for column in range(COUNT_BLOCKS):
-        if column % 2 == 0:
-            color = BLUE
-        else:
-            color = WHITE
-        pygame.draw.rect(screen, color, [10 + column * SIZE_BLOCK + MARGIN, 20, SIZE_BLOCK, SIZE_BLOCK])
+
+    for row in range(COUNT_BLOCKS):
+        for column in range(COUNT_BLOCKS):
+            if (row + column) % 2 == 0:
+                color = BLUE
+            else:
+                color = WHITE
+
+            draw_block(color, row, column)
+
+
+    draw_block(SNAKE_COLOR, 0, 0 )
 
     pygame.display.flip()
-
-
-    jkvbguyhfguttg4
